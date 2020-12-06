82472d5f487176152a50aa62bd189c8ea142672f
chris kye <chriskye@Chris-Kyes-Macbook-2.local>
2020-12-06 13:20:56 +0800

    final edits & ran code

--- src/csprojects1/CSProjectS1.java
+++ src/csprojects1/CSProjectS1.java
@@ -20,9 +20,8 @@ public class CSProjectS1 {
       try {
         Scanner scanner = new Scanner(new File(fileName));
         int currentRow = 0;
-        while (scanner.hasNext()) {
+        while (currentRow < numRows) {
             String[] temp = scanner.nextLine().split(",");
-            System.out.println(temp[0]);
             for (int i = 0; i <numCols; i++) {
                 returnval[currentRow][i] = Double.parseDouble(temp[i]);
             }
@@ -30,17 +29,18 @@ public class CSProjectS1 {
         }
         scanner.close();
       }
+      catch(FileNotFoundException fnfe) {
+          System.out.println("file not found");
+      }
+      catch(ArrayIndexOutOfBoundsException AIOBE) {
+          System.out.println("index out of bounds bruh");
+      }
       catch(Exception e){
       System.out.println("Error doing stuff");
-    }
+      }
       return returnval;
   }
-  
-  
-  
 
-    
-    
     public static double[][] mult(double[][] x, double[][] y) {
         double[][] res = new double[x.length][y[0].length];
         for(int i=0; i < x.length; i++){    
@@ -59,7 +59,7 @@ public class CSProjectS1 {
         double[][] inner = (mult(X, theta));
         double[][] sigmoidHypo = new double[X.length][1];
         for (int i = 0; i < X.length; i++) {
-            sigmoidHypo[i][1] = 1/(1+Math.exp(inner[i][0]));
+            sigmoidHypo[i][0] = 1/(1+Math.exp(inner[i][0]));
         }
         return sigmoidHypo;
     }
@@ -91,11 +91,11 @@ public class CSProjectS1 {
         for (int i = 0; i < num_iter; i++) {
             double[][] h = hypo(X,theta);
             for (int j = 0; j < n; j++) {
-                theta[j][0] -= alpha*grad(X,y,h,j);
+                theta[j][0] += alpha*grad(X,y,h,j); ///TRY TESTING THIS SWITCH TO MINUS
             }
             double[][] h1 = hypo(X,theta);
             double cost = cost(theta, h1, y);
-            System.out.print(cost);
+            System.out.println("Iteration " + (i+1) + ": " + cost);
         }
         return theta;
     }
@@ -110,24 +110,25 @@ public class CSProjectS1 {
             else pred[i] = 0;
             if (pred[i] == y[i][0]) correct++;
         }
+        double accuracy = (double) correct / m;
         System.out.println("Predicted " + correct + " cases, from " + m);
-        System.out.println("Total Accuracy: " + correct/m);
+        System.out.println("Total Accuracy: " + accuracy);
     }
     
     
     //
     public static void main(String[] args) {
         // TODO code application logic here
-        double[][] X = new double[100][2];
-        double[][] y = new double[100][1];
-        double[][] col1 = readCSV("test.csv", 10, 3);
-       
-//        for (int i = 0; i < 10; i++)
-//        {
-//          System.out.println(col1[i][0]);
-//        }
+        double[][] X_testing = readCSV("X_train.csv", 650, 41);
+        double[][] y_testing = readCSV("y_train.csv", 650, 1);
+        double[][] theta = new double[41][1];
+        double[][] newtheta = gradDesc(X_testing, y_testing, theta, 0.1, 8000);
         
+        double[][] X_cool = readCSV("X_test.csv", 239, 41);
+        double[][] y_cool = readCSV("y_test.csv", 239, 1);
         
+        predictor(X_cool, newtheta, y_cool);
+
     }
     
 }
